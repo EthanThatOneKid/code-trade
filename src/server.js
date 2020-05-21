67362eb33045b1e7f7ba38924f1ad372fc538074
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2020-05-20 19:46:29 -0700

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,20 @@
+import sirv from 'sirv';
+import polka from 'polka';
+import compression from 'compression';
+import dotenv from 'dotenv';
+import * as sapper from '@sapper/server';
+dotenv.config();
+
+const { PORT, NODE_ENV, PROJECT_NAME } = process.env;
+const dev = NODE_ENV === 'development';
+
+polka() // You can also use Express
+	.use(
+		PROJECT_NAME,
+		compression({ threshold: 0 }),
+		sirv('static', { dev }),
+		sapper.middleware()
+	)
+	.listen(PORT, err => {
+		if (err) console.log('error', err);
+	});
